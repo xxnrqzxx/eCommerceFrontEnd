52784dee2ab898a0e23ae05e2023fee57be78342
xxnrqzxx <xxnrqzxx@gmail.com>
2020-11-16 20:51:31 +0800

    Uploaded files to github repo

---
+++ Contents.js
@@ -0,0 +1,11 @@
+class Contents extends  React.Component {
+    render() {
+        return(
+            <nav></nav>
+        );
+    }
+
+
+}
+
+ReactDOM.render(<Contents />, document.querySelector('#nav'));
