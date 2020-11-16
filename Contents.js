c6ecb9bf9435e5e23286f17030b6eae2f38d1937
xxnrqzxx <xxnrqzxx@gmail.com>
2020-11-16 23:46:21 +0800

    Nav bar (#3)
    * enhanced navbar hamburger menu on mobile view
    
    * Added media query for navbar hamburger menu to only show on screens between 320px and 420px
    
    * removed comments and active option on nav-items
    
    * removed a comment tag

--- Contents.js
+++ Contents.js
@@ -1,6 +1,5 @@
 class Contents extends  React.Component {
 
-    //test
     render() {
         return(
             <nav className="navbar navbar-expand-lg navbar-dark">
@@ -10,13 +9,13 @@ class Contents extends  React.Component {
                 </button>
                 <div className="collapse navbar-collapse" id="myNavbar">
                     <ul className="navbar-nav">
-                        <li className="nav-item active">
+                        <li className="nav-item">
                             <a className="nav-link" href="#">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">About</a>
                         </li>
-                        <li className="nav-item active">
+                        <li className="nav-item">
                             <a className="nav-link" href="#">Contact</a>
                         </li>
                     </ul>
