7b5e6dc57fe332f500970e3ac8f79829ffa83f33
xxnrqzxx <xxnrqzxx@gmail.com>
2020-11-17 15:04:02 +0800

    updating darkening function when mobile sidebar is collapsed

--- Contents.js
+++ Contents.js
@@ -1,10 +1,28 @@
 class Contents extends  React.Component {
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            darken: false
+        }
+    }
+
+    darkBG() {
+        
+    }
+
     render() {
+        let color = this.state.darken ? "" : "red";
+
+        const styles = {
+            
+        }
+
         return(
             <nav className="navbar navbar-expand-lg navbar-dark">
                 <a href="#" className="navbar-brand">GRAFIX2GO</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar" onClick={this.darkBG}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="myNavbar">
@@ -25,19 +43,25 @@ class Contents extends  React.Component {
     }
 }
 
-document.addEventListener("click", event => {
-    const element = event.target;
-    console.log(element);
+// document.addEventListener("click", event => {
+//     const element = event.target;
+//     console.log(element);
 
-    if(element.classList.contains('navbar-toggler-icon')) {
-        darkenBG();
-    }
-})
+//     if(element.classList.contains('navbar-toggler-icon')) {
+//         darkenBG();
+//     }
+// })
 
 
-function darkenBG() {
-    const bgColor = document.body;
-    const sideBar = document.querySelector('#myNavbar');
-}
+// function darkenBG() {
+//     const bgColor = document.body;
+//     const sideBar = document.querySelector('#myNavbar');
+
+//     if(sideBar.classList.contains("collapsing") && bgColor.style.backgroundColor === "") {
+//         bgColor.style.backgroundColor = "red";
+//     } else if(bgColor.style.backgroundColor === "red"){
+//         bgColor.style.backgroundColor = "";
+//     }
+// }
 
 ReactDOM.render(<Contents />, document.querySelector('#nav'));
