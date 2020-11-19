2c3317904f5a6c40d81ffd49dc634cc9905392e1
xxnrqzxx <xxnrqzxx@gmail.com>
2020-11-19 22:54:58 +0800

    changed X icon to right arrow icon for closing side bar

--- Contents.js
+++ Contents.js
@@ -53,7 +53,9 @@ class Contents extends React.Component {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="myNavbar">
-                        <p className="close-navbar">X</p>
+                        <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-arrow-right close-navbar" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                        </svg>
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <a className="nav-link" href="#">Home</a>
